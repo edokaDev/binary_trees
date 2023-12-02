b97db6cbdbd1c0ff5a482bc1d5becdf9e0544ad7
edoka <peter.edoka.242464@unn.edu.ng>
2023-12-02 13:56:44 +0100

    fix: betty issues

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -13,7 +13,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	
+
 	size = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
 	return (size);
 }
